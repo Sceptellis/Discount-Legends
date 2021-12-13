cdf42abf1b1757bff8f10e0d574cfce4ef9fcab9
rohaid-bakh <rohaidb1999@gmail.com>
2021-12-12 22:21:38 -0500

    Added Key/Gamepad/WASD bindings. Added Jump Movement. Added folders. Created MC prefab

--- Assets/Scripts/MC/Movement.cs
+++ Assets/Scripts/MC/Movement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
@@ -8,27 +9,70 @@ public class Movement : MonoBehaviour
     private CharacterController controller;
     [SerializeField]
     private Transform camera;
-    public float speed = 4f;
+    public Transform transform;
     public float smoothing = 0.1f;
+    public float speed = 4f;
     private float smoothVelocity;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.1f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 2f;
+    private bool isGrounded = false;
+    private Vector3 currVelocity;
+
+    private PlayerInput playerInputActions;
+
 
     void Awake(){
         Cursor.lockState = CursorLockMode.Locked;
+        playerInputActions = new PlayerInput();
+        playerInputActions.Movement.Enable();
+        playerInputActions.Movement.Jump.performed += Jump;
     }
     void Update()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        isGrounded = controller.isGrounded;
         
+        if (isGrounded && currVelocity.y < 0) {
+            //Set to -2 as a default so that even if the player is a Little bit off the ground, they'll move down.
+            currVelocity.y = -2f;
+        }
+    
+
+        Vector2 inputDir = playerInputActions.Movement.Move.ReadValue<Vector2>();
+        Vector3 direction = new Vector3(inputDir.x , 0f, inputDir.y).normalized;
+
+
         if(direction.magnitude >= 0.1f) {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + camera.eulerAngles.y;
-            float smoothAngle  = Mathf.SmoothDampAngle(transform.eulerAngles.y , targetAngle, ref smoothVelocity, smoothing);
-            transform.rotation = Quaternion.Euler(0f, smoothAngle, 0f);
-            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.Move(moveDirection.normalized * speed * Time.deltaTime);
+                    float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + camera.eulerAngles.y; // takes angle of camera direction we want
+                    float smoothAngle  = Mathf.SmoothDampAngle(transform.eulerAngles.y , targetAngle, ref smoothVelocity, smoothing);
+                    transform.rotation = Quaternion.Euler(0f, smoothAngle, 0f);
+                    Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+                    controller.Move(moveDirection.normalized * speed * Time.deltaTime);
         }
 
-
+        currVelocity.y += gravity * Time.deltaTime;
+        controller.Move(currVelocity*Time.deltaTime);
     }
+
+
+    public void Jump(InputAction.CallbackContext context)
+    {   if(context.performed){ // Check for key press not hold
+        if (isGrounded){
+             currVelocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity); //Formula for finding velocity needed to reach a certain height
+        }
+    }
+        
+    }
+
+
+// UNCOMMENT FOR DEBUGGING PURPOSES 
+
+    //  void OnDrawGizmos()
+    // {
+    //      Gizmos.DrawWireSphere(groundCheck.position, groundDistance);
+    // }
+
 }
